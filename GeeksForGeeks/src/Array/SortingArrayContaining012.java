81e7045432fee9fd6072c4fc5af299c6dd0d90fd
amitg <amitg@LAPTOP-CFJVV32J>
2019-04-14 11:19:06 +0530

    Sorting array containing 0/1/2 by self

--- GeeksForGeeks/src/Array/SortingArrayContaining012.java
+++ GeeksForGeeks/src/Array/SortingArrayContaining012.java
@@ -1,10 +1,62 @@
 package Array;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * @author amitg
+ *
+ */
 public class SortingArrayContaining012 {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Size of Array :");
+		int arraySize=sc.nextInt();
+		int intArray[]=new int[arraySize];
+		System.out.println(" Enter the Elements of Array : ");
+		for(int i=0;i<arraySize;i++) {
+			intArray[i]=sc.nextInt();
+		}
+		intArray=sortingArray(intArray,arraySize);
+     System.out.println(" Sorted Array is as below  :");
+       for(int i=0;i<arraySize;i++) {
+    	   System.out.print(intArray[i]+ " ");
+       }
 	}
-
+    public static int[] sortingArray(int[]intArray, int arraySize) {
+    	Map<Integer,Integer> map=new TreeMap<>();
+    	int index=0;
+    	int[] sortedArray=new int[arraySize];
+    	for(int i=0;i<arraySize;i++) {
+    		Integer occurence=map.get(intArray[i]);
+    		if(occurence==null) {
+    			map.put(intArray[i], 1);
+    		}
+    		else {
+    			map.put(intArray[i],++occurence);
+    		}
+    	}
+    	 Set<Entry<Integer,Integer>> setEntry= map.entrySet();
+    	Iterator<Entry<Integer, Integer>> iterator=setEntry.iterator();
+    	 while(iterator.hasNext()) {
+    		 Entry<Integer,Integer> entry=iterator.next();
+    		 Integer key=entry.getKey();
+    		 Integer value=entry.getValue();
+    		 System.out.println(" key="+key +"  "+" value="+value);
+    		 for(int i=0;i<value;i++) {
+    			 sortedArray[index]=key;
+    			 index++;
+    		 }
+    	  }
+    	 return sortedArray;
+    }
 }
